808cd1ad4d5a195e116cbe8da74495f7c728602b
Sugimoto Masayoshi <sugimoto.massayoshi@gmail.com>
2019-01-14 21:24:41 +0900

    Refactoring

--- js1k.js
+++ js1k.js
@@ -1,56 +1,60 @@
-canvasElement = document.createElement('canvas')
-canvas = canvasElement.getContext('2d')
-document.body.appendChild(canvasElement)
+//a=vectorAdd
+//c=canvas.getContext('2d')
+//d=document
+//z=document.createElement('canvas')
+//v=vector constructor
 
-vector = (x,y) => ({x:x,y:y})
-
-vectorAdd = (v1, v2) => vector(v1.x+v2.x, v1.y+v2.y)
-
-rotationMatrix = angle => {
-	let cosine = Math.cos(angle)	
-	let sinus = Math.sin(angle)
+d=document
+z=d.createElement('canvas')
+c=z.getContext('2d')
+d.body.appendChild(z)
+v=(x,y)=>({x:x,y:y})
+a=(v1,v2)=>v(v1.x+v2.x,v1.y+v2.y)
+rotationMatrix=angle => {
+	let cosine=Math.cos(angle)	
+	let sinus=Math.sin(angle)
 	return [
-		[cosine, -sinus],
-		[sinus, cosine]
+		[cosine,-sinus],
+		[sinus,cosine]
 	]
 }
 
-matrixMultiply = (m, v) => {
-	return vector(
-		(m[0][0]*v.x)+(m[0][1]*v.y),
-		(m[1][0]*v.x)+(m[1][1]*v.y)
+matrixMultiply=(m,w)=>{
+	return v(
+		(m[0][0]*w.x)+(m[0][1]*w.y),
+		(m[1][0]*w.x)+(m[1][1]*w.y)
 	)
 }
 
-equilateralTriangle = (center, side, rotation) => {
-	dv = vector(0, side)
+equilateralTriangle=(center,side,rotation)=>{
+	dv=v(0,side)
 	return [
-		vectorAdd(center, matrixMultiply(rotationMatrix(rotation), vector(0, -side))),
-		vectorAdd(center, matrixMultiply(rotationMatrix(rotation+(2*Math.PI/6)), dv)),
-		vectorAdd(center, matrixMultiply(rotationMatrix(rotation-(2*Math.PI/6)), dv))
+		a(center,matrixMultiply(rotationMatrix(rotation),v(0,-side))),
+		a(center,matrixMultiply(rotationMatrix(rotation+(2*Math.PI/6)),dv)),
+		a(center,matrixMultiply(rotationMatrix(rotation-(2*Math.PI/6)),dv))
 	]
 }
 
-drawTriangle = (x,y,side,rotation) => {
-	triangle = equilateralTriangle(vector(x, y), side, rotation)
-	canvas.fillStyle=randomRGB()
-	canvas.beginPath()
-	canvas.moveTo(triangle[0].x,triangle[0].y)
-	canvas.lineTo(triangle[1].x,triangle[1].y)
-	canvas.lineTo(triangle[2].x,triangle[2].y)
-	canvas.fill()
+drawTriangle=(x,y,side,rotation)=>{
+	triangle=equilateralTriangle(v(x,y),side,rotation)
+	c.fillStyle=randomRGB()
+	c.beginPath()
+	c.moveTo(triangle[0].x,triangle[0].y)
+	c.lineTo(triangle[1].x,triangle[1].y)
+	c.lineTo(triangle[2].x,triangle[2].y)
+	c.fill()
 }
 
-randomComponent = () => Math.floor(Math.random() * 256)
+randomComponent=()=>Math.floor(Math.random()*256)
 
-randomRGB = () => {
-	return `rgb(${randomComponent()}, ${randomComponent()}, ${randomComponent()})`
+randomRGB=()=>{
+	return `rgb(${randomComponent()},${randomComponent()},${randomComponent()})`
 }
 
-rot = 0
-draw = () => {
-	rot += 0.01
-	drawTriangle(100, 100, 50, rot)
+rot=0
+draw=()=>{
+	rot+=0.01
+	drawTriangle(100,100,50,rot)
 	requestAnimationFrame(draw)
 }
 
