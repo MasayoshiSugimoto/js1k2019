cc42dd6d3bfa28cb88e29d83817da3eec84ce339
Sugimoto Masayoshi <sugimoto.massayoshi@gmail.com>
2019-02-01 23:47:41 +0900

    Optimize. Within 1k

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -6,17 +6,18 @@ Q=M.sqrt
 //v = Vector constructor
 v=(x,y)=>({x,y})
 //A: add
-A=(v1,v2)=>v(v1.x+v2.x,v1.y+v2.y)
+A=(w,z)=>v(w.x+z.x,w.y+z.y)
 //C: clamp
-C=(value,max)=>M.min(max,M.max(value,0))
+C=(a,b)=>M.min(b,M.max(a,0))
 //X: multiply
-X=(vector,scalar)=>v(vector.x*scalar,vector.y*scalar)
+X=(w,s)=>v(w.x*s,w.y*s)
 
+//o: oldPosition
 //p: position
 //r: radius
 //a: acceleration
 //MP: makeParticle
-MP=(p,r)=>({oldPosition:p,p:p,a:v(0,0),radius:r})
+MP=(p,r)=>({o:p,p,a:v(0,0),r})
 //ScreenWidth
 W=a.width
 //ScreenHeight
@@ -27,8 +28,11 @@ P=Q(W*H/900)
 //PS: Particles
 PS=[]
 F=_=>PS.forEach(_)
+T=(x,y)=>MP(X(A(X(v(W-150,H-300),.5),v(x,y)),1/P),.5)
 for(i=1;i<300;i++)
-	PS[i]=MP(v(R()*W/P,R()*H/P),0.5)
+	PS[i]=i<150
+		?T(i,2*i)
+		:T(i-150,(300-2*(i-150)))
 
 a.onclick=e=>PS[0]=MP(v(e.clientX/P,e.clientY/P),2)
 
@@ -44,53 +48,55 @@ S=_=>c.fillStyle=_
 //1000/60/1000
 D=.017
 setInterval(()=>{
-	//Apply force
-	//10 is the G force
-	F(_=>_.a=v(0,10))
+	if(PS[0]){
+		//Apply force
+		//10 is the G force
+		F(_=>_.a=v(0,10))
 
-	//Verlet
-	F(particle=>{
-		T=A(
-			particle.p,
-			A(A(particle.p,X(particle.oldPosition,-1)),X(particle.a,D*D))
-		)
-		particle.oldPosition=particle.p
-		particle.p=T
-	})
-
-	//Update collisions
-	F(p1=>{
-		F(p2=>{
-			if(p1!=p2){
-				let delta=A(p2.p,X(p1.p,-1))
-				let deltaLength=Q(delta.x*delta.x+delta.y*delta.y)
-				let restLength=p1.radius+p2.radius
-				if(deltaLength>0.01 && deltaLength<restLength){
-					let diff=(restLength-deltaLength)/deltaLength
-					p1.p=A(p1.p,X(delta,-diff*(p2.radius/restLength)))
-					p2.p=A(p2.p,X(delta,diff*(p1.radius/restLength)))
-				}
-			}
+		//Verlet
+		F(particle=>{
+			T=A(
+				particle.p,
+				A(A(particle.p,X(particle.o,-1)),X(particle.a,D*D))
+			)
+			particle.o=particle.p
+			particle.p=T
 		})
 
-		p1.p.x=C(p1.p.x,W/P)
-		p1.p.y=C(p1.p.y,H/P)
-	})
+		//Update collisions
+		F(p1=>{
+			F(p2=>{
+				if(p1!=p2){
+					let delta=A(p2.p,X(p1.p,-1))
+					let deltaLength=Q(delta.x*delta.x+delta.y*delta.y)
+					let restLength=p1.r+p2.r
+					if(deltaLength>0.01 && deltaLength<restLength){
+						let diff=(restLength-deltaLength)/deltaLength
+						p1.p=A(p1.p,X(delta,-diff*(p2.r/restLength)))
+						p2.p=A(p2.p,X(delta,diff*(p1.r/restLength)))
+					}
+				}
+			})
+
+			p1.p.x=C(p1.p.x,W/P)
+			p1.p.y=C(p1.p.y,H/P)
+		})
+
+		PS[0]=PS[1]
+	}
 
 	S("black")
 	c.fillRect(0,0,W,H)
 	S(G)
 
-	F(particle=>{
+	F(_=>{
 		c.beginPath()
 		c.arc(
-			particle.p.x*P,
-			particle.p.y*P,
-			particle.radius*P*2,
+			_.p.x*P,
+			_.p.y*P,
+			_.r*P*2,
 			0,
 			2*M.PI)
 		c.fill()
 	})
-
-	PS[0]=PS[1]
 },D*1000)
